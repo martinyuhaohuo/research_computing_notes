20ed5c5993b351501e239a1857d2d90ab21fbb4a
Yuhao Huo <martinyuhaohuo@gmail.com>
2025-11-15 01:14:20 +0000

    update second week notes

---
+++ activate_env.sh
@@ -0,0 +1,4 @@
+# Source the Python virtual environment
+source ~/Desktop/code/rc_class/rc_venvs/rc_computing_notes/bin/activate
+# Print a message to confirm the environment is set
+echo "Environment variables are set, and the virtual environment is activated."
